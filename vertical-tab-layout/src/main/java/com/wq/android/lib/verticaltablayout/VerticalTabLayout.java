54db4888bcdafdac5d243e3b76d3d1a89a5068fd
kim-wang-bj <wangqi.bj@gmail.com>
2016-08-17 16:07:52 +0800

    fix bugs.

--- vertical-tab-layout/src/main/java/com/wq/android/lib/verticaltablayout/VerticalTabLayout.java
+++ vertical-tab-layout/src/main/java/com/wq/android/lib/verticaltablayout/VerticalTabLayout.java
@@ -16,6 +16,7 @@ import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -593,6 +594,7 @@ public class VerticalTabLayout extends ScrollView implements View.OnClickListene
             }
             mTabItem = new TextView(mContext);
             mTabItem.setText(mTab.getText());
+            mTabItem.setTextSize(TypedValue.COMPLEX_UNIT_PX, mTabTextSize);
             mTabItem.setTextColor(mTabTextColor);
             mTabItem.setGravity(Gravity.CENTER_VERTICAL);
             setPaddings();
